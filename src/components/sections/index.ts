291952bc5d9db580b896c8ab7251a26f101dc1b7
Dreck Sallow <arandadikson@gmail.com>
2024-10-08 17:58:39 -0500

    feat: split the index page into section and add some components

---
+++ src/components/sections/index.ts
@@ -0,0 +1,3 @@
+export { default as ExperienceSection } from "./experience.astro";
+export { default as ProjectsSection } from "./projects.astro";
+export { default as ContactSection } from "./contact.astro";
