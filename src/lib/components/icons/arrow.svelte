7864d059ecdc5df672d0168aa4c9b953ffd18735
Dikson Aranda <arandadikson@gmail.com>
2025-06-16 16:41:20 -0500

    add arrow and world icons

---
+++ src/lib/components/icons/arrow.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	type Direction = 'left' | 'right';
+	export let direction: Direction = 'right';
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	class={['arrow-icon icon', `arrow-direction-${direction}`]}
+	viewBox="0 0 24 24"
+>
+	<path d="m11.293 17.293 1.414 1.414L19.414 12l-6.707-6.707-1.414 1.414L15.586 11H6v2h9.586z"
+	></path>
+</svg>
+
+<style>
+	.arrow-icon:is(.arrow-direction-left) {
+		transform: translateX(180deg);
+	}
+</style>
--- src/lib/components/icons/index.ts
+++ src/lib/components/icons/index.ts
@@ -3,3 +3,5 @@ export { default as CalendarIcon } from "./calendar.svelte";
 export { default as SocialIcon } from "./social.svelte";
 export { default as MailIcon } from "./mail.svelte";
 export { default as MenuIcon } from "./menu.svelte";
+export { default as ArrowIcon } from "./arrow.svelte";
+export { default as WorldIcon } from "./world.svelte";
