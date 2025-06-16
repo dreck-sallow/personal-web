f3a2a40b0b8f27d25f394b33454af74bf4d617bc
Dikson Aranda <arandadikson@gmail.com>
2025-06-16 16:41:59 -0500

    refactor: add card component for project detail

---
+++ src/lib/components/project-card.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+	import { ArrowIcon, SocialIcon, WorldIcon } from './icons';
+
+	export let imgUrl: string;
+	export let imgAlt: string;
+	export let name: string;
+
+	export let githubLink: string | undefined = undefined;
+	export let websiteLink: string | undefined = undefined;
+</script>
+
+<div class="project-card">
+	<img class="project-card__image" src={imgUrl} alt={imgAlt} />
+
+	<div class="project-card__content">
+		<h4 class="project-card__title app-heading-text-bold">{name}</h4>
+		<p class="project-card__desc app-text-regular">
+			<slot />
+		</p>
+	</div>
+
+	{#if githubLink || websiteLink}
+		<div class="project-card__links">
+			{#if githubLink}
+				<a
+					class="project-card__github-link app-text-regular"
+					aria-label="Github repository"
+					href={githubLink}
+					target="_blank"
+				>
+					<SocialIcon name="github" />
+					Github
+					<ArrowIcon />
+				</a>
+			{/if}
+
+			{#if websiteLink}
+				<a
+					class="project-card__web-link app-text-regular"
+					aria-label="Project website"
+					href={websiteLink}
+					target="_blank"
+				>
+					<WorldIcon />
+					Web
+
+					<ArrowIcon />
+				</a>
+			{/if}
+		</div>
+	{/if}
+</div>
+
+<style>
+	.project-card {
+		--max-description-width: initial;
+		--image-width: initial;
+		--image-height: initial;
+		--image-aspect-ratio: initial;
+
+		background-color: hsla(var(--th-color-on-bg), 1);
+		border-radius: 1rem;
+		padding: 2rem;
+	}
+
+	.project-card__image {
+		width: var(--image-width);
+		height: var(--image-height);
+		aspect-ratio: var(--image-aspect-ratio);
+		border-radius: 0.5rem;
+	}
+
+	.project-card__content .project-card__title {
+		color: hsla(var(--th-color-title), 1);
+		margin-block-start: 1rem;
+		margin-block-end: 1rem;
+		font-size: 1.25rem;
+	}
+
+	.project-card__content .project-card__desc {
+		color: hsla(var(--th-color-text), 0.9);
+		max-width: var(--max-description-width);
+	}
+
+	.project-card__links {
+		margin-top: 1rem;
+		display: flex;
+		flex-direction: row;
+		gap: 1rem;
+	}
+
+	.project-card__links :is(.project-card__github-link, .project-card__web-link) {
+		display: flex;
+		align-items: center;
+		flex-direction: row;
+		gap: 0.3rem;
+
+		font-size: 1rem;
+	}
+</style>
--- src/lib/components/projects-grid.svelte
+++ src/lib/components/projects-grid.svelte
@@ -1,91 +1,54 @@
+<script>
+	import ProjectCard from './project-card.svelte';
+</script>
+
 <div class="projects-grid">
-	<div class="project-card">
-		<img
-			class="project-card__image"
-			src="https://diksoncode.com/_astro/quipp-image.D24gA1-j_Z24EWD9.webp"
-			alt="Quipp app"
-		/>
-		<h4 class="project-card__title app-heading-text-bold">Quipp</h4>
-		<p class="project-card__desc app-text-regular">
-			🌐 A community-driven platform for interactive discussions, ideal for live streams and Q&A
-			sessions.
-		</p>
-	</div>
+	<ProjectCard
+		imgUrl="https://diksoncode.com/_astro/quipp-image.D24gA1-j_Z24EWD9.webp"
+		imgAlt="Quipp app"
+		name="Quipp"
+		githubLink="https://github.com/DreckSallow/qa-web"
+		websiteLink="https://qa-web.vercel.app/"
+	>
+		🌐 A community-driven platform for interactive discussions, ideal for live streams and Q&A
+		sessions.
+	</ProjectCard>
 
-	<div class="project-card">
-		<img
-			class="project-card__image"
-			src="https://diksoncode.com/_astro/flix-markdown.CEWhFR_V_gGm5X.webp"
-			alt="Flix"
-		/>
-		<h4 class="project-card__title app-heading-text-bold">Flix</h4>
-		<p class="project-card__desc app-text-regular">
-			🖥️ An open-source desktop app that integrates Anki decks with note-taking for enhanced study
-			sessions.
-		</p>
-	</div>
+	<ProjectCard
+		imgUrl="https://diksoncode.com/_astro/flix-markdown.CEWhFR_V_gGm5X.webp"
+		imgAlt="Flix desktop"
+		name="Flix"
+		githubLink="https://github.com/DreckSallow/flix"
+		websiteLink="https://flixdev.netlify.app/"
+	>
+		🖥️ An open-source desktop app that integrates Anki decks with note-taking for enhanced study
+		sessions.
+	</ProjectCard>
 
-	<div class="project-card">
-		<img
-			class="project-card__image"
-			src="https://diksoncode.com/_astro/prj-melody.B6fr3rcL_oqHa4.webp"
-			alt="Melody program"
-		/>
-		<h4 class="project-card__title app-heading-text-bold">Melody</h4>
-		<p class="project-card__desc app-text-regular">
-			🎧 A terminal-based music player (TUI) with keyboard navigation, designed for efficiency and
-			minimalism.
-		</p>
-	</div>
+	<ProjectCard
+		imgUrl="https://diksoncode.com/_astro/prj-melody.B6fr3rcL_oqHa4.webp"
+		imgAlt="Melody program"
+		name="Melody"
+		githubLink="https://github.com/DreckSallow/melody"
+	>
+		🎧 A terminal-based music player (TUI) with keyboard navigation, designed for efficiency and
+		minimalism.
+	</ProjectCard>
 
-	<div class="project-card">
-		<img
-			class="project-card__image"
-			src="https://diksoncode.com/_astro/flow-img.Cnk6rY7c_ZhgoMW.webp"
-			alt="Flow cli"
-		/>
-		<h4 class="project-card__title app-heading-text-bold">Flow</h4>
-		<p class="project-card__desc app-text-regular">
-			✅ A CLI task manager for streamlined project organization and workflow optimization.
-		</p>
-	</div>
+	<ProjectCard
+		imgUrl="https://diksoncode.com/_astro/flow-img.Cnk6rY7c_ZhgoMW.webp"
+		imgAlt="Flow cli"
+		name="Flow"
+		githubLink="https://github.com/DreckSallow/flow"
+	>
+		✅ A CLI task manager for streamlined project organization and workflow optimization.
+	</ProjectCard>
 </div>
 
 <style>
 	.projects-grid {
 		display: grid;
-		/* grid-template-columns: repeat(5, 12rem);
-		grid-auto-rows: repeat(4, 12rem); */
 		gap: 1.5rem;
-	}
-
-	.project-card {
-		--max-description-width: initial;
-		--image-width: initial;
-		--image-height: initial;
-		--image-aspect-ratio: initial;
-
-		background-color: hsla(var(--th-color-on-bg), 1);
-		border-radius: 1rem;
-		padding: 2rem;
-	}
-
-	.project-card__image {
-		border-radius: 0.5rem;
-		width: var(--image-width);
-		height: var(--image-height);
-		aspect-ratio: var(--image-aspect-ratio);
-	}
-
-	.project-card__title {
-		color: hsla(var(--th-color-title), 1);
-		margin-block-start: 1rem;
-		margin-block-end: 1rem;
-	}
-
-	.project-card__desc {
-		color: hsla(var(--th-color-text), 0.9);
-		max-width: var(--max-description-width);
 	}
 
 	@media (width <= 40rem) {
@@ -95,7 +58,7 @@
 		}
 	}
 
-	@media (width <= 64rem) {
+	@media (width <= 64rem) and (width > 40rem) {
 		.projects-grid {
 			grid-template-columns: repeat(2, minmax(12rem, 28rem));
 		}
@@ -108,33 +71,33 @@
 		}
 
 		/* Apply grid styles for each item */
-		.project-card:nth-of-type(1) {
+		.projects-grid :global(.project-card:nth-of-type(1)) {
 			grid-row: 1 / 2;
 			grid-column: 1 / 4;
 		}
 
-		.project-card:nth-of-type(2) {
+		.projects-grid :global(.project-card:nth-of-type(2)) {
 			grid-row: 1 / 2;
 			grid-column: 4 / 6;
 		}
 
-		.project-card:nth-of-type(3) {
+		.projects-grid :global(.project-card:nth-of-type(3)) {
 			grid-row: 2 / 3;
 			grid-column: 1 / 3;
 		}
 
-		.project-card:nth-of-type(4) {
+		.projects-grid :global(.project-card:nth-of-type(4)) {
 			grid-row: 2 / 3;
 			grid-column: 3 / 6;
 		}
 
-		.project-card:is(:nth-of-type(1), :nth-of-type(4)) {
+		.projects-grid :global(.project-card:is(:nth-of-type(1), :nth-of-type(4))) {
 			--max-description-width: 23rem;
 			--image-width: 100%;
 			--image-aspect-ratio: 16 / 9;
 		}
 
-		.project-card:is(:nth-of-type(2), :nth-of-type(3)) {
+		.projects-grid :global(.project-card:is(:nth-of-type(2), :nth-of-type(3))) {
 			--image-width: 100%;
 			--image-height: 20rem;
 		}
