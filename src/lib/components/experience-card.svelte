09b9fc03df536435e0b043fd7a99fccd1402ac7e
Dikson Aranda <arandadikson@gmail.com>
2025-06-20 18:48:09 -0500

    feat: use a component facade for timeline card

--- src/lib/components/experience-card.svelte
+++ src/lib/components/experience-card.svelte
@@ -1,96 +1,27 @@
 <script lang="ts">
-	import { CalendarIcon, CompanyIcon } from './icons';
+	// import { CalendarIcon, CompanyIcon } from './icons';
+	import TimelineCard from './timeline-card.svelte';
+
+	type Direction = 'left' | 'right';
+	type Position = 'start' | 'end' | 'center';
 
 	export let tag: 'li' | 'div' = 'li';
-	export let position: string;
-	export let company: string;
+	export let place: `${Direction}-${Position}` = 'right-center';
+
+	export let title: string;
 	export let date: string;
+
+	$: position = place.split('-')[1] as Position;
+	$: direction = place.split('-')[0] as Direction;
 </script>
 
-<svelte:element this={tag} class="experience-card flex-col">
-	<header class="experience-card__header">
-		<h2 class="experience-card__title app-heading-text-bold">
-			{position}
-		</h2>
-
-		<div class="experience-card__header-info flex-row">
-			<CompanyIcon />
-			<span class="experience-card__header-info__text">
-				{company}
-			</span>
-		</div>
-
-		<div class="experience-card__header-info flex-row">
-			<CalendarIcon />
-			<span class="experience-card__header-info__text">
-				{date}
-			</span>
-		</div>
-	</header>
-	<div class="experience-card__description">
+<TimelineCard {tag} {direction} {position}>
+	<span slot="header">{title}</span>
+	<span slot="label">{date}</span>
+	<p class="app-text-regular">
 		<slot />
-	</div>
-</svelte:element>
+	</p>
+</TimelineCard>
 
 <style>
-	.experience-card {
-		gap: 0.5rem;
-		position: relative;
-		max-width: 45rem;
-		border: 1px solid hsla(var(--th-color-accent-text), 0.5);
-		border-radius: 1.5rem;
-		padding-inline: 2rem;
-		padding-block: 2.1rem;
-		background-color: hsla(var(--th-color-on-bg), 1);
-	}
-
-	.experience-card::before {
-		--size: 8px;
-		content: '';
-		width: var(--size);
-		border-radius: 1000px;
-		aspect-ratio: 1;
-		background-color: hsla(var(--th-color-text), 1);
-		position: absolute;
-		transform: translateX(calc(var(--size) * -3));
-		top: 0;
-		left: 0;
-	}
-
-	.experience-card::after {
-		--size: 8px;
-		content: '';
-		width: 1px;
-		background-color: hsla(var(--th-color-accent-text), 0.5);
-		position: absolute;
-		transform: translateX(calc(var(--size) * -3 + 4px));
-		top: calc(var(--size) + 2px);
-		left: 0;
-		bottom: 0;
-		height: calc(100% + var(--size));
-	}
-
-	.experience-card__title {
-		color: hsla(var(--th-color-title-text), 1);
-		font-size: 1.2rem;
-		margin-bottom: 0.25rem;
-	}
-
-	.experience-card__header-info {
-		color: hsla(var(--th-color-text), 0.75);
-		align-items: center;
-		gap: 0.275rem;
-		/* FIXME: apply direct styles to svg like size, to combine with info__text*/
-	}
-
-	.experience-card__header-info__text {
-		font-family: var(--th-text-font);
-		font-weight: 400;
-		font-style: normal;
-		font-size: 0.875rem;
-	}
-
-	.experience-card__description {
-		margin-top: 0.5rem;
-	}
 </style>
--- src/lib/components/sections/experience.svelte
+++ src/lib/components/sections/experience.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { OBSERVED_SELECTOR } from '$lib/pages/principal/section-observer';
-	// import ExperienceCard from '../experience-card.svelte';
-	import TimelineCard from '../timeline-card.svelte';
+	import ExperienceCard from '../experience-card.svelte';
+	// import TimelineCard from '../timeline-card.svelte';
 </script>
 
 <section
@@ -17,44 +17,48 @@
 	</header>
 
 	<ul class="section-experience__timeline">
-		<TimelineCard tag="li" position="left" time="start">
-			<span slot="header">FullStack developer - Arisale</span>
-			<span slot="label">Nov 2023 - Mar 2023</span>
-			<p>
-				I used Vue with Nuxt.js to create web projects and employed SASS for styling. I collaborated
-				with the backend team to add features and resolve issues using Nest.js.
-			</p>
-		</TimelineCard>
+		<ExperienceCard
+			tag="li"
+			place="left-start"
+			title="FullStack developer - Arisale"
+			date="Nov 2023 - Mar 2023"
+		>
+			I used Vue with Nuxt.js to create web projects and employed SASS for styling. I collaborated
+			with the backend team to add features and resolve issues using Nest.js.
+		</ExperienceCard>
 
-		<TimelineCard tag="li" time="inside" position="right">
-			<span slot="header">Frontend developer - Liquid</span>
-			<span slot="label">Nov 2023 - Mar 2023</span>
-			<p>
-				I developed web applications using Vue, Nuxt, Pinia, and Tailwind CSS, and deployed them on
-				Google Cloud Platform (GCP). I worked closely with the design team to streamline workflows
-				and enhance user experience.
-			</p>
-		</TimelineCard>
+		<ExperienceCard
+			tag="li"
+			place="right-center"
+			title="Frontend developer - Liquid"
+			date="Nov 2023 - Mar 2023"
+		>
+			I developed web applications using Vue, Nuxt, Pinia, and Tailwind CSS, and deployed them on
+			Google Cloud Platform (GCP). I worked closely with the design team to streamline workflows and
+			enhance user experience.
+		</ExperienceCard>
 
-		<TimelineCard tag="li" time="inside" position="left">
-			<span slot="header">Frontend Developer - Kimche</span>
-			<span slot="label">Jun 2022 - Aug 2022</span>
-			<p>
-				I implemented new features in web applications, focusing on data visualization through
-				tables, charts, and progress bars. I identified and fixed issues, improving functionality
-				and user experience. Technologies used: React, React Context, and Cube.js.
-			</p>
-		</TimelineCard>
+		<ExperienceCard
+			tag="li"
+			place="left-center"
+			title="Frontend Developer - Kimche"
+			date="Jun 2022 - Aug 2022"
+		>
+			I implemented new features in web applications, focusing on data visualization through tables,
+			charts, and progress bars. I identified and fixed issues, improving functionality and user
+			experience. Technologies used: React, React Context, and Cube.js.
+		</ExperienceCard>
 
-		<TimelineCard tag="li" time="end" position="right">
-			<span slot="header">FullStack Developer (Education) - SoyHenry</span>
-			<span slot="label">Oct 2021 - Mar 2022</span>
-			<p>
-				I completed Soy Henry's Bootcamp, where I gained skills in full-stack web development within
-				the JavaScript ecosystem (frontend and backend). For the final project, we developed an
-				online tech store. Skills acquired: React, Redux, Node.js, PostgreSQL, HTML, CSS, Sequelize.
-			</p>
-		</TimelineCard>
+		<ExperienceCard
+			tag="li"
+			place="right-end"
+			title="FullStack Developer (Education) - SoyHenry"
+			date="Oct 2021 - Mar 2022"
+		>
+			I completed Soy Henry's Bootcamp, where I gained skills in full-stack web development within
+			the JavaScript ecosystem (frontend and backend). For the final project, we developed an online
+			tech store. Skills acquired: React, Redux, Node.js, PostgreSQL, HTML, CSS, Sequelize.
+		</ExperienceCard>
 	</ul>
 </section>
 
